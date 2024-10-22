446bc72e407d2653dc13d2fa4b8ade546b60521c
Mayureshh-9 <mayureshchikane6@gmail.com>
2024-10-23 00:36:25 +0530

    updated

---
+++ BestStoreMVC/Services/ApplicationDbcontext.cs
@@ -0,0 +1,15 @@
+﻿using BestStoreMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BestStoreMVC.Services
+{
+    public class ApplicationDbcontext : DbContext // Corrected the class name to PascalCase
+    {
+        public ApplicationDbcontext(DbContextOptions options) : base(options)
+        {
+            // No need to set Products here; Entity Framework does this automatically
+        }
+
+        public DbSet<Product> Products { get; set; } // Ensure class name is capitalized
+    }
+}
